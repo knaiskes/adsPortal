289ea7e1b5cee6001d2810643d1023ccaf5199b2
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-11-21 22:27:48 +0200

    Add __str__ function to ad model

--- ads/models.py
+++ ads/models.py
@@ -4,3 +4,6 @@ class Ad(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=120)
     pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
