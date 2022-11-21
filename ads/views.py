adcafb60c31b4babaa11c6be85955a53372f0549
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-11-21 23:16:15 +0200

    Render index html template

--- ads/views.py
+++ ads/views.py
@@ -1,11 +1,17 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import Ad
 
 def index(request):
     latest_ad_list = Ad.objects.order_by('-pub_date')[:5]
     output = ', '.join([a.title for a in latest_ad_list])
-    return HttpResponse(output)
+    templete = loader.get_template('ads/index.html')
+    context = {
+        'latest_ad_list': latest_ad_list
+    }
+    return HttpResponse(templete.render(context, request))
 
 def detail(request, ad_id):
     return HttpResponse("This is the details page for: %s" % ad_id)
