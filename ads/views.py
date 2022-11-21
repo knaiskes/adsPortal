81cd7523d35abcfa52d92328cce3a6b66cbe9134
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-11-21 22:56:51 +0200

    Add the latest ads in the index page

--- ads/views.py
+++ ads/views.py
@@ -1,5 +1,11 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Ad
 
 def index(request):
-    return HttpResponse("Hello from ads")
+    latest_ad_list = Ad.objects.order_by('-pub_date')[:5]
+    output = ', '.join([a.title for a in latest_ad_list])
+    return HttpResponse(output)
+
+def detail(request, ad_id):
+    return HttpResponse("This is the details page for: %s" % ad_id)
