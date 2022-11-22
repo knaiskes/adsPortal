dcdf887db7bedb3363075cc475d991bb27668648
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-11-22 23:23:05 +0200

    Add app_name (namespace) to ads's urls.py

--- ads/urls.py
+++ ads/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .import views
 
+app_name = 'ads'
+
 urlpatterns =[
     path('', views.index, name='index'),
     path('<int:ad_id>/', views.detail, name='detail'),
