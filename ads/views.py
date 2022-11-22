f10354728c04183c2f8628232ea62d3cb306a4c5
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-11-22 22:32:27 +0200

    Use render instead of loader

--- ads/views.py
+++ ads/views.py
@@ -1,17 +1,14 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Ad
 
 def index(request):
     latest_ad_list = Ad.objects.order_by('-pub_date')[:5]
     output = ', '.join([a.title for a in latest_ad_list])
-    templete = loader.get_template('ads/index.html')
-    context = {
-        'latest_ad_list': latest_ad_list
-    }
-    return HttpResponse(templete.render(context, request))
+    context = { 'latest_ad_list': latest_ad_list }
+    return render(request, 'ads/index.html', context)
 
 def detail(request, ad_id):
     return HttpResponse("This is the details page for: %s" % ad_id)
