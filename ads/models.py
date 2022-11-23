3afaf836b54727d9a0ee40b969b7aa08ea189680
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-11-23 22:12:03 +0200

    Add phone field to Ad model

--- ads/models.py
+++ ads/models.py
@@ -10,7 +10,8 @@ class Ad(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=120)
     pub_date = models.DateTimeField('date published')
-    ad_type = models.ForeignKey(Type, on_delete=models.CASCADE, null=True)
+    phone = models.CharField(max_length=20)
+    ad_type = models.ForeignKey(Type, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
