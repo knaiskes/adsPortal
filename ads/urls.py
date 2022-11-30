5360fdbcd1f4f226b4f92f09eadff2a527f2da94
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-11-30 23:08:47 +0200

    Add image field in ads model

--- ads/urls.py
+++ ads/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'ads'
 
@@ -7,3 +9,5 @@ urlpatterns =[
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
