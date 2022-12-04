574af01bed4ba7cf40d37a9830022f7137871e69
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-12-04 17:20:55 +0200

    Add price field to ad model

--- ads/models.py
+++ ads/models.py
@@ -12,6 +12,7 @@ class Ad(models.Model):
     pub_date = models.DateTimeField('date published')
     phone = models.CharField(max_length=20)
     image = models.ImageField(upload_to='images/')
+    price = models.CharField(max_length=25, default='0')
     ad_type = models.ForeignKey(Type, on_delete=models.CASCADE)
 
     def __str__(self):
