6c56e5a9045eab2a89b15bad84403c5a2488cc13
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-12-10 00:35:28 +0200

    Read .env.dev file in settings.py

--- adsPortal/settings.py
+++ adsPortal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +22,12 @@ MEDIA_URL = '/media/'
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d^msc3mj_0*ya_b)!o8$r#nw(0@ut0ia+=x@k$yog-4fjx_wun'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-d^msc3mj_0*ya_b)!o8$r#nw(0@ut0ia+=x@k$yog-4fjx_wun')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', default='localhost').split(' ')
 
 # Application definition
 
