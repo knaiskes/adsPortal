c4db903ee4980dd661594710df6c4a709905e082
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-12-25 22:30:04 +0200

    Add forms.py with PostAdform

--- ads/models.py
+++ ads/models.py
@@ -9,11 +9,11 @@ class Type(models.Model):
 class Ad(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=1000)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(auto_now_add=True, blank=True)
     phone = models.CharField(max_length=20)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
     price = models.CharField(max_length=25, default='0')
-    ad_type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    ad_type = models.ForeignKey(Type, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.title
--- ads/views.py
+++ ads/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.views import generic
+from django.views.generic.edit import FormView
+
+from ads.forms import PostAdForm
 
 from .models import Ad
 
@@ -18,9 +21,15 @@ class DetailView(generic.DetailView):
     template_name = 'ads/detail.html'
 
 
-class PostAdView(generic.ListView):
-    model = Ad
+class PostAdView(FormView):
+    #model = Ad
     template_name = 'ads/add.html'
+    form_class = PostAdForm
+    success_url = '/ads/post-ad'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
 
 class AboutView(generic.ListView):
